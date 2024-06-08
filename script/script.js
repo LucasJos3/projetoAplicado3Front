d3729feb1134335387d02a5235ebe34f022ec137
LucasJos3 <lucasjosedepaula@hotmail.com>
2024-06-08 11:18:38 -0300

    projetoaplicado3

---
+++ script/script.js
@@ -0,0 +1,30 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function toggleDropdown() {
+  document.getElementById("userDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".user-icon")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function logout() {
+  // Implementar a lógica de logout aqui
+  alert("Logout realizado!");
+}
