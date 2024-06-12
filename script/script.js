70c6157639f4b5fd52d039fd6c9b3dd9387dbdb9
LucasJos3 <lucasjosedepaula@hotmail.com>
2024-06-12 18:29:42 -0300

    att2

--- script/script.js
+++ script/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:8080/product")
+    .then((response) => response.json())
+    .then((data) => {
+      const productContainer = document.getElementById("product-container");
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("product");
+
+      const productName = document.createElement("h2");
+      productName.textContent = data.name;
+
+      const productDescription = document.createElement("p");
+      productDescription.textContent = data.description;
+
+      productDiv.appendChild(productName);
+      productDiv.appendChild(productDescription);
+
+      productContainer.appendChild(productDiv);
+    })
+    .catch((error) => console.error("Error fetching product data:", error));
+});
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -28,25 +50,3 @@ function logout() {
   // Implementar a lógica de logout aqui
   alert("Logout realizado!");
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("http://localhost:8080/product")
-    .then((response) => response.json())
-    .then((data) => {
-      const productContainer = document.getElementById("product-container");
-      const productDiv = document.createElement("div");
-      productDiv.classList.add("product");
-
-      const productName = document.createElement("h2");
-      productName.textContent = data.name;
-
-      const productDescription = document.createElement("p");
-      productDescription.textContent = data.description;
-
-      productDiv.appendChild(productName);
-      productDiv.appendChild(productDescription);
-
-      productContainer.appendChild(productDiv);
-    })
-    .catch((error) => console.error("Error fetching product data:", error));
-});
