d1c2d16b9c0e05ee9ea9e4db622496167ac6a017
LucasJos3 <lucasjosedepaula@hotmail.com>
2024-06-24 18:42:11 -0300

    ATT/APONTAMENTO

--- script/script_apontamento.js
+++ script/script_apontamento.js
@@ -1,4 +1,3 @@
-
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -30,7 +29,6 @@ function logout() {
   alert("Logout realizado!");
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("production-form");
   let submitting = false;
@@ -47,17 +45,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const formData = new FormData(form);
 
     fetch("http://localhost:8080/production", {
-      method: 'post',
+      method: "post",
       body: JSON.stringify(Object.fromEntries(formData)),
       headers: {
-        'Content-Type': 'application/json;application/x-www-form-urlencoded'
-      }
+        "Content-Type": "application/json;application/x-www-form-urlencoded",
+      },
     })
       .then((response) => {
         submitting = false;
 
         if (!response.ok) {
-          throw new Error("Não foi possível cadastrar Apontamento: " + response.statusText);
+          throw new Error(
+            "Não foi possível cadastrar Apontamento: " + response.statusText
+          );
         }
         alert("Apontamento cadastrado com sucesso!");
         return response.json(); // Retorna os dados do backend como JSON
@@ -70,6 +70,63 @@ document.addEventListener("DOMContentLoaded", function () {
         submitting = false;
         alert("Erro ao cadastrar Apontamento: " + error.message);
         form.reset();
+      });
+  });
+});
+
+//ATUALIZAR APONTAMENTO
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("production-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const planQuantity = document.getElementById("planQuantity").value;
+    const realQuantity = document.getElementById("realQuantity").value;
+    const unit = document.getElementById("unit").value;
+    const startTime = document.getElementById("startTime").value;
+    const finishTime = document.getElementById("finishTime").value;
+    const downtime = document.getElementById("downtime").value;
+    const packageType = document.getElementById("packageType").value;
+    const labelType = document.getElementById("labelType").value;
+    const equipment = document.getElementById("equipment").value;
+    const bestBefore = document.getElementById("bestBefore").value;
+    const notes = document.getElementById("notes").value;
+
+    const apontamentoData = {
+      planQuantity: planQuantity,
+      realQuantity: realQuantity,
+      unit: unit,
+      startTime: startTime,
+      finishTime: finishTime,
+      downtime: downtime,
+      packageType: packageType,
+      labelType: labelType,
+      equipment: equipment,
+      bestBefore: bestBefore,
+      notes: notes,
+    };
+
+    fetch(`http://localhost:8080/product/${productId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json;application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(apontamentoData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.planQuantity != null) {
+          alert("Apontamento atualizado com sucesso!");
+        } else {
+          alert("Falha ao atualizar o apontamento: " + data.message);
+        }
+        document.getElementById("production-form").reset();
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+        alert("Ocorreu um erro ao atualizar o apontamento." + data.message);
       });
   });
 });
